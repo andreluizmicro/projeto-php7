907d744c0bca50d546122b40fa83d1b25a1c67db
Andre Luiz <andreluizmicro@gmail.com>
2021-06-16 14:31:14 -0300

    criado os metodos de listagem dos produtos

--- index.php
+++ index.php
@@ -2,14 +2,34 @@
 
 require_once("config.php");
 
+###############################################################################################################
 // $sql = new Sql();
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 // echo json_encode($usuarios);
 
 
-$usuario = new Usuario();
+###############################################################################################################
+// CARREGA UM USUÁRIO PELO ID
+// $usuario = new Usuario();
+// $usuario->loadById(3);
+// echo $usuario;
 
-$usuario->loadById(3);
+
+###############################################################################################################
+// CAREREGA UMA LISTA DE USUÁRIOS
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+
+###############################################################################################################
+// CARREGA UMA LISTA DE USUÁRIOS BUSCANDO PELO LOGIN
+// $search = Usuario::search("jo");
+// echo json_encode($search);
+
+###############################################################################################################
+// CARREGA UM USUÁRIO USANDO O LOGIN E A SENHA
+$usuario = new Usuario();
+$usuario->login("root", "!@#123saa21");
 
 echo $usuario;
 
